a89925b343291aa4258e959b85f43c29cd18e7c7
Aryan Sharma <agsharma98@gmail.com>
2024-09-17 03:15:50 -0700

    Initial Commit

---
+++ test2/test2App.swift
@@ -0,0 +1,17 @@
+//
+//  test2App.swift
+//  test2
+//
+//  Created by Aryan Sharma on 17/09/24.
+//
+
+import SwiftUI
+
+@main
+struct test2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
